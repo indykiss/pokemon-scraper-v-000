2eaa5eb78972084d0bf90b87b956b7ed1036d195
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-12-20 21:46:42 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -14,7 +14,7 @@ class Pokemon
 	
 	def self.find(id, db)
     new_pokemon_stats = db.execute("SELECT * FROM Pokemon WHERE id = ?",id)
-    Pokemon.new(new_pokemon_stats)
+    Pokemon.new(new_pokemon_stats[0],(new_pokemon_stats[1], (new_pokemon_stats[2] )
   end
 	
 end
