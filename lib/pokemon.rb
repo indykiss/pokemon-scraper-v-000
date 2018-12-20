2161cf0220a05c3d1b4d44555eec15c320d13e08
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-12-20 21:07:13 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,20 @@
 class Pokemon
-end
+ 	attr_accessor :name, :type, :id, :db 
+
+	def initialize(id)
+	  @db = db
+	  @id = id
+  	@name = name
+  	@type = type
+	end
+	
+	def self.save(name, type, db)
+    db.execute("INSERT INTO Pokemon (name, type) VALUES (?, ?)",name, type)
+  end
+	
+	def self.find(type, name, db)
+    db.execute("SELECT type, name FROM Pokemon",type,name)
+  end
+	
+end 
+
